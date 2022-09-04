edd8de43af4bbf24bc6f81f641f89559694dbdeb
chathushka <chathushkag@gmail.com>
2022-09-05 05:25:18 +0900

    #change main directory name

--- filestorage/src/main/java/com/chathushka/file/storage/repository/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chathushka.file.storage.repository;
-
-import com.chathushka.file.storage.entity.FileEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository extends PagingAndSortingRepository<FileEntity, String> {}
